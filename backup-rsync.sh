206e3d20bea4bbc5862c9f554c605f84f03adb37
Matheus Fidelis <msfidelis@users.noreply.github.com>
2015-10-30 19:25:56 -0200

    Create backup-rsync.sh

---
+++ backup-rsync.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#BACKUP CRIADO POR: Matheus Fidelis 
+#Github - https://github.com/msfidelis/
+#Twitter - @fidelisauro 
+#Blog - http://www.nanoshots.com.br
+
+
+#SCRIPT DE BACKUP DIFERENCIAL REALIZADO NO FILESERVER
+
+#VARIAVEIS
+INICIO=`date +%d/%m/%Y-%H:%M:%S`
+LOG=/var/log/backups/full/`date +%Y-%m-%d`_log-backup-full.txt
+
+#DEFINA AQUI O DIRETÓRIO QUE SERÁ EFETUADO O BACKUP
+ORIGEM=/mnt/dados/
+
+#DEFINA AQUI O DIRETÓRIO ONDE O ARQUIVO SERÁ GRAVADO JUNTO COM O SEU NOME
+DESTINO=/mnt/hdexterno/backup-`date +%Y-%m-%d`.tar.gz
+
+#CRIA O ARQUIVO DE LOGS
+echo " " >> $LOG
+echo " " >> $LOG
+echo "|-----------------------------------------------" >> $LOG
+echo " Sincronização iniciada em $INICIO" >> $LOG
+
+
+#CRIA O BACKUP
+
+rsync -Cravzp $ORIGEM $DESTINO >> $LOG
+
+
+FINAL=`date +%d/%m/%Y-%H:%M:%S`
+
+echo " Sincronização Finalizada em $FINAL" >> $LOG
+echo "|-----------------------------------------------" >> $LOG
+echo " " >> $LOG
+echo " " >> $LOG
+
+
+#FIM DO SCRIPTS
